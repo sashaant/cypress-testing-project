1323de8ddb054419af11a512756fdea28f16a997
sashaant <antonenko.qa@gmail.com>
2021-02-27 19:39:02 +0200

    write tests for Sign in page

---
+++ cypress/integration/my_tests/login.js
@@ -0,0 +1,30 @@
+describe('working with inputs', () => {
+    it('should load login page',() => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+    })
+    it('should fill username ', () => {
+            cy.get('#user_login').clear()
+            cy.get('#user_login').type('Some Invalid Name')
+    })
+
+    it('should fill password ', () => {
+        cy.get('#user_password').clear()
+        cy.get('#user_password').type('Some Invalid password')
+       
+    })
+
+    it('should mark checkbox', () => {
+        cy.get('input[type="checkbox"]').click()
+        cy.wait(5000)
+    })
+    
+    it('should submit login form', () => {
+        cy.contains('Sign in').click()
+    })
+
+    it('should display error message', () => {
+        cy.get('.alert-error').should('be.visible')
+    })
+})
+
+
