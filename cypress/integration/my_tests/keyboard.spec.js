ecd1258bcbcf50d831c8a98d7b3d63e6afb07d10
sashaant <antonenko.qa@gmail.com>
2021-03-02 00:43:42 +0200

    Keyboard press simulation

---
+++ cypress/integration/my_tests/keyboard.spec.js
@@ -0,0 +1,8 @@
+describe('Keyboard press simulation', () => {
+    it('should submit searchbox with pressing enter', () => {
+    
+        cy.visit('http://zero.webappsecurity.com/index.html')
+        cy.get('#searchTerm').type('just some text {enter}')
+
+    })
+})
