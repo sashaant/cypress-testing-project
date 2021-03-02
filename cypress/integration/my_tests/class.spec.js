fddda30d217bff10847ff2e7ef1b2e562a5ae181
sashaant <antonenko.qa@gmail.com>
2021-03-02 18:15:49 +0200

    Abstraction with Classes

---
+++ cypress/integration/my_tests/class.spec.js
@@ -0,0 +1,32 @@
+class BasePage{
+    static loadHomePage() {
+        cy.visit('https://devexpress.github.io/testcafe/example/')
+    }
+
+    static wait(number) {
+        cy.wait(number)
+    }
+}
+
+class HomePage extends BasePage {
+    static scrollToBotton() {
+        cy.get('#submit-button').scrollIntoView()
+    }
+
+    static scrollToTop() {
+        cy.get('header').scrollIntoView()
+
+    }
+    
+}
+
+describe('Abstraction with Classes', () => {
+    it('should scroll down and up on the page', () =>{
+       HomePage.loadHomePage()
+       HomePage.scrollToBotton()
+       HomePage.wait(5000)
+       HomePage.scrollToTop()
+       HomePage.wait(5000)
+
+    })
+})
