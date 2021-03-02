f1ca1212b009be212a757f8bd3cbe9871c64509c
sashaant <antonenko.qa@gmail.com>
2021-03-02 17:40:24 +0200

    Read Data from JSON / Local Data Files

--- cypress/integration/my_tests/data.spec.js
+++ cypress/integration/my_tests/data.spec.js
@@ -1,3 +1,5 @@
+const { isTaggedTemplateExpression } = require("typescript")
+
 describe('Write/Read Data to JSON / Text file', () => {
     it('should write data into JSON', () => {
         cy.writeFile('log.json', { name: "Mike", age: 25 })
@@ -5,5 +7,15 @@ describe('Write/Read Data to JSON / Text file', () => {
 
     it('should write data into text file', () => {
         cy.writeFile('log.txt', 'Hello World')
+    })
+
+    it('should read and verify data from JSON file', () => {
+        cy.readFile('log.json')
+            .its('age')
+            .should('eq', 25) 
+    })
+
+   it('should read and verify data from TEXT file', () => {
+        cy.readFile('log.txt').should('eq', 'Hello World')
     })
 })
