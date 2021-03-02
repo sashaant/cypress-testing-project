2289f5f5cbc2eba0f943a4dbbd3ac9dbe74f7386
sashaant <antonenko.qa@gmail.com>
2021-03-02 18:30:14 +0200

    Before & After Hooks

--- cypress/integration/my_tests/class.spec.js
+++ cypress/integration/my_tests/class.spec.js
@@ -16,11 +16,35 @@ class HomePage extends BasePage {
     static scrollToTop() {
         cy.get('header').scrollIntoView()
 
-    }
-    
+    }  
 }
 
 describe('Abstraction with Classes', () => {
+
+    before(function (){
+        //runs before all test inside describe 
+        //setup or test context
+        //seed or reset the database
+        HomePage.loadHomePage
+    })
+
+    after(function () {
+        //runs after all tests inside describe blockare done
+        //test clean up
+        //clean cookies or localStorage
+    })
+
+
+    beforeEach(function () {
+        //runs before each it block in the describe  
+    })
+
+    afterEach(function () {
+        //runs after each it block in the describe  
+    })
+
+
+
     it('should scroll down and up on the page', () =>{
        HomePage.loadHomePage()
        HomePage.scrollToBotton()
